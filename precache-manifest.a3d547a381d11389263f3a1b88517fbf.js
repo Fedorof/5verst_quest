19f3600a19720a3537f6892ed2a7c61428751d20
Oleksandr Fedorov <a.g.fedorof@gmail.com>
2018-10-21 10:14:08 +0300

    Updates

---
+++ precache-manifest.a3d547a381d11389263f3a1b88517fbf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "315982d93f78279b6405",
+    "url": "/hardway/static/js/runtime~main.315982d9.js"
+  },
+  {
+    "revision": "3bd8a2b2a5a949fb40d8",
+    "url": "/hardway/static/js/main.3bd8a2b2.chunk.js"
+  },
+  {
+    "revision": "1de2eb3053d9743aa46f",
+    "url": "/hardway/static/js/1.1de2eb30.chunk.js"
+  },
+  {
+    "revision": "3bd8a2b2a5a949fb40d8",
+    "url": "/hardway/static/css/main.6828902e.chunk.css"
+  },
+  {
+    "revision": "822f0428feebb96be981d7b0a1add1a6",
+    "url": "/hardway/index.html"
+  }
+];
